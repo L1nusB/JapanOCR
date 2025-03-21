7cb1dc4250780a1cae94419d1cf3a59bf8475f44
Linus <47142578+L1nusB@users.noreply.github.com>
2025-03-21 14:41:32 +0100

    Fix permission issues

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,28 +1,13 @@
 #!/bin/bash
 set -e
 
-echo "Activating base environment..."
+echo "Setting up workspace..."
 # Activate the base environment
-source /opt/conda/bin/activate base-env
-
-echo "Installing additional packages (mamba)..."
-# Install the remaining packages
-mamba install -y -c conda-forge \
-  pdf2image \
-  opencv \
-  poppler \
-  ipywidgets \
-  matplotlib \
-  black \
-  pylint \
-  mypy
-
-echo "Installing additional packages (pip)..."
-# Install pip packages
-pip install pytest pytest-cov
-# pip install -r requirements.txt
+# echo "Activating base environment"
+# source /opt/conda/bin/activate base-env
 
 echo "Testing Tesseract installation..."
+python -c "import pytesseract; print('Tesseract OCR setup successful!')"
 tesseract --version
 
 echo "Creating example files directory..."
